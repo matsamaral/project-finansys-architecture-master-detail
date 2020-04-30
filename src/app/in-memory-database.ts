36cd9fb38858001ea17dc12b891f01baefa08c0b
Mateus Augusto Amaral <mamaral@viceri.com.br>
2020-04-30 16:55:30 -0300

    Adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,15 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb() {
+        const categories = [
+            { id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa.' },
+            { id: 2, name: 'Saúde', description: 'Plano de saúde e remédios.' },
+            { id: 3, name: 'Lazer', description: 'Cinema, parque, praia e etc.' },
+            { id: 4, name: 'Salário', description: 'Recebimento de salário.' },
+            { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer.' }
+        ];
+
+        return { categories };
+    }
+}
