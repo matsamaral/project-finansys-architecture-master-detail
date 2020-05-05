5c13c493193ea9a926c7db972181cbcbbe382ac0
Mateus Augusto Amaral <mamaral@viceri.com.br>
2020-05-05 16:07:39 -0300

    Category service

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,10 @@
-import { InMemoryDbService } from "angular-in-memory-web-api";
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             { id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa.' },
             { id: 2, name: 'Saúde', description: 'Plano de saúde e remédios.' },
             { id: 3, name: 'Lazer', description: 'Cinema, parque, praia e etc.' },
