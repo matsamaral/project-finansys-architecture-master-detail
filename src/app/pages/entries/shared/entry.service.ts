0d85d0729150840e77c2590269648a0e207376ae
Mateus Augusto Amaral <mamaral@viceri.com.br>
2020-05-10 19:54:27 -0300

    Adding baseresourceformcomponent

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -2,7 +2,7 @@ import { CategoryService } from './../../categories/shared/category.service';
 import { Injectable, Injector } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, catchError } from 'rxjs/operators';
 
 import { Entry } from './entry.model';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
@@ -20,22 +20,21 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-
-        return super.create(entry);
-      })
-    );
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
 
-        return super.update(entry);
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
     );
   }
 
---
+++ src/app/shared/components/base-resource-form/base-resource-form.component.ts
@@ -0,0 +1,140 @@
+import { OnInit, AfterContentChecked, Injector } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { BaseResourceModel } from './../../models/base-resource.model';
+import { BaseResourceService } from './../../services/base-resource.service';
+
+import { switchMap, take } from 'rxjs/operators';
+import toastr from 'toastr';
+
+export abstract class BaseResourceFormComponent<T extends BaseResourceModel> implements OnInit, AfterContentChecked {
+
+    currentAction: string;
+    resourceForm: FormGroup;
+    pageTitle: string;
+    serverErrorMessages: string[] = null;
+    submittingForm = false;
+
+    protected route: ActivatedRoute;
+    protected router: Router;
+    protected formBuilder: FormBuilder;
+
+    constructor(
+        protected injector: Injector,
+        public resource: T,
+        protected resourceService: BaseResourceService<T>,
+        protected jsonDataToResourceFn: (jsonData: any) => T
+    ) {
+        this.route = this.injector.get(ActivatedRoute);
+        this.router = this.injector.get(Router);
+        this.formBuilder = this.injector.get(FormBuilder);
+    }
+
+    ngOnInit() {
+        this.setCurrentAction();
+        this.buildResourceForm();
+        this.loadResource();
+    }
+
+    ngAfterContentChecked() {
+        this.setPageTitle();
+    }
+
+    submitForm() {
+        this.submittingForm = true;
+
+        if (this.currentAction === 'new') {
+            this.createResource();
+        } else {
+            this.updateResource();
+        }
+    }
+
+    // Private Methods
+    protected setCurrentAction() {
+        if (this.route.snapshot.url[0].path === 'new') {
+            this.currentAction = 'new';
+        } else {
+            this.currentAction = 'edit';
+        }
+    }
+
+    protected loadResource() {
+        if (this.currentAction === 'edit') {
+            this.route.paramMap.pipe(
+                switchMap(params => this.resourceService.getById(+params.get('id')))
+            ).subscribe(
+                (resource) => {
+                    this.resource = resource;
+                    this.resourceForm.patchValue(this.resource); // Binds loaded resource data to resourceForm
+                },
+                (error) => alert('Ocorreu um erro no servidor, tente novamente mais tarde!')
+            );
+        }
+    }
+
+    protected setPageTitle() {
+        if (this.currentAction === 'new') {
+            this.pageTitle = this.creationPageTitle();
+        } else {
+            this.pageTitle = this.editionPageTitle();
+        }
+
+    }
+
+    protected creationPageTitle(): string {
+        return 'Novo';
+    }
+
+    protected editionPageTitle(): string {
+        return 'Edição';
+    }
+
+    protected createResource() {
+        const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+        this.resourceService.create(resource).pipe(
+            take(1)
+        ).subscribe(
+            (result) => this.actionsForSuccess(result),
+            (error) => this.actionsForError(error)
+        );
+    }
+
+    protected updateResource() {
+        const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+        this.resourceService.update(resource).pipe(
+            take(1)
+        ).subscribe(
+            (result) => this.actionsForSuccess(result),
+            (error) => this.actionsForError(error)
+        );
+    }
+
+    // Redirect / reload component page
+    protected actionsForSuccess(resource: T) {
+        toastr.success('Solicitação processada com sucesso!');
+
+        const baseComponentPath: string = this.route.snapshot.parent.url[0].path;
+
+        this.router.navigateByUrl(`/${baseComponentPath}`, { skipLocationChange: true }).then(
+            () => this.router.navigate([baseComponentPath, resource.id, 'edit'])
+        );
+    }
+
+    protected actionsForError(error) {
+        toastr.error('Ocorreu um erro ao processar a sua solicitação!');
+
+        this.submittingForm = false;
+
+        if (error.status === 422) {
+            this.serverErrorMessages = JSON.parse(error._body).errors;
+        } else {
+            this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, tente novamente mais tarde.'];
+        }
+    }
+
+    protected abstract buildResourceForm(): void;
+}
